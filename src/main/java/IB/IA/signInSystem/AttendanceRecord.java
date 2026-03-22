344773be623eb8ca36c7a2becb97f90bb1918c5a
Omicron17 <prajeetvawesome@gmail.com>
2026-03-21 21:35:03 -0600

    attendance record class
    Updated attendance record class, changed UML diagrams to include additional methods, not sure if this one is done yet and will come back if necessary

--- src/main/java/IB/IA/signInSystem/AttendanceRecord.java
+++ src/main/java/IB/IA/signInSystem/AttendanceRecord.java
@@ -1,4 +1,45 @@
 package src.main.java.IB.IA.signInSystem;
 
+import java.time.LocalDateTime;
+
 public class AttendanceRecord {
+    private String studentId;
+    private LocalDateTime signInTime;
+    private LocalDateTime signOutTime;
+    private boolean autoLoggedOut;
+
+    public AttendanceRecord(String studentId, LocalDateTime signInTime, LocalDateTime signOutTime) {
+        this.studentId = studentId;
+        this.signInTime = signInTime;
+        this.signOutTime = signOutTime;
+        this.autoLoggedOut = false;
+    }
+
+    public String getStudentId() {
+        return this.studentId;
+    }
+
+    public LocalDateTime getSignInTime() {
+        return this.signInTime;
+    }
+
+    public LocalDateTime getSignOutTime() {
+        return this.signOutTime;
+    }
+
+    public boolean isAutoLoggedOut() {
+        return this.autoLoggedOut;
+    }
+
+    public void setAutoLoggedOut(boolean autoLoggedOut) {
+        this.autoLoggedOut = autoLoggedOut;
+    }
+
+    public void setSignOutTime(LocalDateTime signOutTime) {
+        this.signOutTime = signOutTime;
+    }
+
+    public void setSignInTime(LocalDateTime signInTime) {
+        this.signInTime = signInTime;
+    }
 }
